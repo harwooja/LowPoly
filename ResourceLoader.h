6f24ae4f8d0e26e4322cb67d1cfb98eb38f321a3
Simon <berhas@mcmaster.ca>
2014-12-18 16:37:10 -0500

    fixed a few errors with lists, added new files to makefile

--- ResourceLoader.h
+++ ResourceLoader.h
@@ -6,17 +6,17 @@
 #include <vector>
 
 class ResourceLoader {
-   
+
     /***************************************
     *    PUBLIC FUNCTIONS
     **************************************/
     public:
-    
+
         GLubyte* loadPPM(char* fileName, bool pathRelative, int* width, int* height);
         void loadPPMTexture(char* fileName, bool pathRelative, GLuint* textures);
         float** loadPPMHeightmap(char* fileName, bool pathRelative, int terrainSize);
         float** convertImageToHeightmap(GLubyte* image, int heightmapRadius);
-        void loadObj(char *fileName, bool pathRelative, std::vector<std::vector<float>>* vertices, std::vector<std::vector<float>>* normals, std::vector<std::vector<int>>* faceIndices, std::vector<std::vector<int>>* normalIndices);
+        void loadObj(char *fileName, bool pathRelative, std::vector<std::vector<float> >* vertices, std::vector<std::vector<float> >* normals, std::vector<std::vector<int> >* faceIndices, std::vector<std::vector<int> >* normalIndices);
 
 
 };
